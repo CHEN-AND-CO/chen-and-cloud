e6ea58fbf56949cf473cdad46722c95e17a74949
BlivionIaG <kev29lt@gmail.com>
2018-03-21 19:26:33 +0100

    modifié :         php/request.php

--- php/request.php
+++ php/request.php
@@ -8,35 +8,35 @@
     }
 
     function authenticate($db){
-        $login = $_SERVER['PHP_AUTH_USER'];
-        $pass = $_SERVER['PHP_AUTH_PW'];
+      $login = $_SERVER['PHP_AUTH_USER'];
+      $pass = $_SERVER['PHP_AUTH_PW'];
 
-        if(!dbCheckUserInjection($db, $login, $pass)){
-            header('HTTP/1.1 401 Unauthorized');
-            exit;
-        }
+      if(!dbCheckUserInjection($db, $login, $pass)){
+          header('HTTP/1.1 401 Unauthorized');
+          exit;
+      }
 
-        $token = base64_encode(openssl_random_pseudo_bytes(12));
-        dbAddToken($db, $login, $token);
-        header('Content-Type: text/plain; charset=utf-8');
-        header('Cache-control: no-store, no-cache, must-revalidate');
-        header('Pragma: no-cache');
-        echo $token;
-        
+      $token = base64_encode(openssl_random_pseudo_bytes(12));
+      dbAddToken($db, $login, $token);
+      header('Content-Type: text/plain; charset=utf-8');
+      header('Cache-control: no-store, no-cache, must-revalidate');
+      header('Pragma: no-cache');
+      echo $token;
+      
+      exit;
+  }
+
+  function verifyToken($db){
+    $headers = getallheaders();
+    $token = $headers['Authorization'];
+    if (preg_match('/Bearer (.*)/', $token, $tab))$token = $tab[1];
+    if(!($login = dbVerifyToken($db, $token))){
+        header('HTTP/1.1 401 Unauthorized');
         exit;
     }
 
-    function verifyToken($db){
-        $headers = getallheaders();
-        $token = $headers['Authorization'];
-        if (preg_match('/Bearer (.*)/', $token, $tab))$token = $tab[1];
-        if(!($login = dbVerifyToken($db, $token))){
-            header('HTTP/1.1 401 Unauthorized');
-            exit;
-        }
-
-        return $login;
-    }
+    return $login;
+}
 
     $db = dbConnect();
     if (!$db)
@@ -49,9 +49,34 @@
     $request = substr($_SERVER['PATH_INFO'], 1);
     $request = explode('/', $request);
     $requestRessource = array_shift($request);
-    
-    if($requestRessource == 'authenticate'){
-        authenticate($db);
+
+    $id = array_shift($request);
+    if ($id == '')
+      $id = NULL;
+    $data = false;
+
+    if ($requestRessource == 'photos'){
+        if($id == NULL){
+            $output = dbRequestPhotos($db);
+        }else{
+            $output = dbRequestPhoto($db, intval($id));
+        }
+
+        if($output != false){
+            sendJsonData($output, 'HTTP/1.1 200 OK');
+        }
+    }else if($requestRessource == 'comments'){
+        if($requestType == 'GET'){
+            $output = dbRequestComments($db, intval($id));
+        }else if($requestType == 'POST'){
+            $output = dbAddComment($db, intval($id), intval($id), $_POST('comment'));
+        }else if($requestType == 'DELETE'){
+            $output = dbDeleteComment($db, intval($id), intval($id));
+        }
+    }else if($requestRessource == 'authenticate'){
+      authenticate($db);
+    }else{
+        header('HTTP/1.1 400 Bad Request');
     }
 
     $login = verifyToken($db);
