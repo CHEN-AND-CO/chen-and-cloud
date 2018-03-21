77f5c6a28c366ee65494c5e6a46da9d172ae286c
BlivionIaG <kev29lt@gmail.com>
2018-03-21 16:12:08 +0100

    nouveau fichier : archivekevin/css/bootstrap-grid.css 	nouveau fichier : archivekevin/css/bootstrap-grid.css.map 	nouveau fichier : archivekevin/css/bootstrap-grid.min.css 	nouveau fichier : archivekevin/css/bootstrap-grid.min.css.map 	nouveau fichier : archivekevin/css/bootstrap-reboot.css 	nouveau fichier : archivekevin/css/bootstrap-reboot.css.map 	nouveau fichier : archivekevin/css/bootstrap-reboot.min.css 	nouveau fichier : archivekevin/css/bootstrap-reboot.min.css.map 	nouveau fichier : archivekevin/css/bootstrap.css 	nouveau fichier : archivekevin/css/bootstrap.css.map 	nouveau fichier : archivekevin/css/bootstrap.min.css 	nouveau fichier : archivekevin/css/bootstrap.min.css.map 	renommé :         css/global.css -> archivekevin/css/global.css 	nouveau fichier : archivekevin/fonts/glyphicons-halflings-regular.woff2 	renommé :         img/chen.png -> archivekevin/img/chen.png 	renommé :         img/large/photo1.png -> archivekevin/img/large/photo1.png 	renommé :         img/large/photo10.png -> archivekevin/img/large/photo10.png 	renommé :         img/large/photo11.png -> archivekevin/img/large/photo11.png 	renommé :         img/large/photo12.png -> archivekevin/img/large/photo12.png 	renommé :         img/large/photo2.png -> archivekevin/img/large/photo2.png 	renommé :         img/large/photo3.png -> archivekevin/img/large/photo3.png 	renommé :         img/large/photo4.png -> archivekevin/img/large/photo4.png 	renommé :         img/large/photo5.png -> archivekevin/img/large/photo5.png 	renommé :         img/large/photo6.png -> archivekevin/img/large/photo6.png 	renommé :         img/large/photo7.png -> archivekevin/img/large/photo7.png 	renommé :         img/large/photo8.png -> archivekevin/img/large/photo8.png 	renommé :         img/large/photo9.png -> archivekevin/img/large/photo9.png 	renommé :         img/small/photo1.png -> archivekevin/img/small/photo1.png 	renommé :         img/small/photo10.png -> archivekevin/img/small/photo10.png 	renommé :         img/small/photo11.png -> archivekevin/img/small/photo11.png 	renommé :         img/small/photo12.png -> archivekevin/img/small/photo12.png 	renommé :         img/small/photo2.png -> archivekevin/img/small/photo2.png 	renommé :         img/small/photo3.png -> archivekevin/img/small/photo3.png 	renommé :         img/small/photo4.png -> archivekevin/img/small/photo4.png 	renommé :         img/small/photo5.png -> archivekevin/img/small/photo5.png 	renommé :         img/small/photo6.png -> archivekevin/img/small/photo6.png 	renommé :         img/small/photo7.png -> archivekevin/img/small/photo7.png 	renommé :         img/small/photo8.png -> archivekevin/img/small/photo8.png 	renommé :         img/small/photo9.png -> archivekevin/img/small/photo9.png 	copié :           index.html -> archivekevin/index.html 	nouveau fichier : archivekevin/js/ajax.js 	renommé :         js/authentication.js -> archivekevin/js/authentication.js 	nouveau fichier : archivekevin/js/bootstrap.bundle.js 	nouveau fichier : archivekevin/js/bootstrap.bundle.js.map 	nouveau fichier : archivekevin/js/bootstrap.bundle.min.js 	nouveau fichier : archivekevin/js/bootstrap.bundle.min.js.map 	nouveau fichier : archivekevin/js/bootstrap.js 	nouveau fichier : archivekevin/js/bootstrap.js.map 	nouveau fichier : archivekevin/js/bootstrap.min.js 	nouveau fichier : archivekevin/js/bootstrap.min.js.map 	nouveau fichier : archivekevin/js/jquery-3.3.1.js 	nouveau fichier : archivekevin/js/js.cookie.js 	renommé :         php/constants.php -> archivekevin/php/constants.php 	renommé :         php/database.php -> archivekevin/php/database.php 	renommé :         php/request.php -> archivekevin/php/request.php 	nouveau fichier : archivekevin/sql/sql.sql 	nouveau fichier : css/main.css 	modifié :         index.html 	nouveau fichier : js/chat.js

---
+++ archivekevin/js/ajax.js
@@ -0,0 +1,93 @@
+/**
+ * @Author: Thibault NapolÃ©on <Imothep>
+ * @Company: ISEN YncrÃ©a Ouest
+ * @Email: thibault.napoleon@isen-ouest.yncrea.fr
+ * @Created Date: 23-Jan-2018 - 17:00:53
+ * @Last Modified: 24-Jan-2018 - 17:03:23
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+//--- ajaxRequest --------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Perform an Ajax request.
+// \param type The type of the request (GET, DELETE, POST, PUT).
+// \param request The request with the data.
+// \param callback The callback to call where the request is successful.
+// \param data The data associated with the request.
+function ajaxRequest(type, request, callback, data = null) {
+  var xhr;
+
+  // Create XML HTTP request.
+  xhr = new XMLHttpRequest();
+  if (type == 'GET' && data != null)
+    request += '?' + data;
+  xhr.open(type, request, true);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + Cookies.get('token'));
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // Add the onload function.
+  xhr.onload = function () {
+    switch (xhr.status) {
+      case 200:
+      case 201:
+        console.log(xhr.responseText);
+        callback(xhr.responseText);
+        break;
+      case 401:
+      case 403:
+        httpErrors(xhr.status);
+        authentication();
+        break;
+      default:
+        httpErrors(xhr.status);
+    }
+  };
+
+  // Send XML HTTP request.
+  xhr.send(data);
+}
+
+//------------------------------------------------------------------------------
+//--- httpErrors ---------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Display a message corresponding to an Http error code.
+// \param errorNumber the error code.
+function httpErrors(errorNumber) {
+  var text;
+
+  text = '<div class="alert alert-danger" role="alert">';
+  text += '<span class="glyphicon glyphicon-exclamation-sign"></span>';
+  switch (errorNumber) {
+    case 400:
+      // Bad request.
+      text += '<strong> RequÃªte incorrecte</strong>';
+      break;
+    case 401:
+      // Unauthorized.
+      text += '<strong> Authentifiez vous</strong>';
+      break;
+    case 403:
+      // Forbidden.
+      text += '<strong> AccÃ¨s refusÃ©</strong>';
+      break;
+    case 404:
+      // Ressource not found.
+      text += '<strong> Page non trouvÃ©e</strong>';
+      break;
+    case 500:
+      // Internal server error.
+      text += '<strong> Erreur interne du serveur</strong>';
+      break;
+    case 503:
+      // Service unavailable.
+      text += '<strong> Service indisponible</strong>';
+      break;
+    default:
+      text += '<strong> HTTP erreur ' + errorNumber + '</strong>';
+      break;
+  }
+  text += '</div>';
+  $('#errors').html(text);
+}
