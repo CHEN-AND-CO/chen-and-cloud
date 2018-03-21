6658fe68c6239a4ace57e3e0d7ad2cd848f20a97
BlivionIaG <kev29lt@gmail.com>
2018-03-21 18:28:15 +0100

    nouveau fichier : js/authentication.js 	modifié :         js/chat.js 	nouveau fichier : php/constants.php 	nouveau fichier : php/database.php 	nouveau fichier : php/request.php 	nouveau fichier : php/sql.sql

--- js/chat.js
+++ js/chat.js
@@ -10,52 +10,49 @@ websocket.onopen = () => {
     websocket.send(login + " s'est connecté");
 };
 
-websocket.onerror = function (er)
-{
-    console.error("Unable to open websocket connection on "+websocket.url);
+websocket.onerror = function (er) {
+    console.error("Unable to open websocket connection on " + websocket.url);
 }
 
 websocket.onmessage = (event) => {
-    $('#chat ul.msg-list').append('<li class="message">'+event.data+'</li>');  
-    
+    $('#chat ul.msg-list').append('<li class="message">' + event.data + '</li>');
+
     $("#chat ul.msg-list").finish();
     $("#chat ul.msg-list").animate({ scrollTop: $('#chat ul.msg-list').prop("scrollHeight") }, 1000);
 };
 
 
 
-$( () => {
+$(() => {
 
-    $('#chat-input').submit( (ev) => {
+    $('#chat-input').submit((ev) => {
         let msg = $('#chat-input input').val();
         console.log("message : " + msg);
 
         ev.preventDefault();
 
-        websocket.send(login+' : '+msg);
+        websocket.send(login + ' : ' + msg);
 
         $('#chat-input input').val('');
     });
-    
-    $('#chat h2').click( (ev) => {
+
+    $('#chat h2').click((ev) => {
         console.log('click');
-        
+
         toggleSpam = !toggleSpam;
 
-        if (toggleSpam)
-        {
+        if (toggleSpam) {
             $('#chat ul').css('background-color', 'red');
 
-            spammer = setInterval( () => {
+            spammer = setInterval(() => {
                 websocket.send(spamString);
             }, 50);
-        } 
-        else 
-        {
+        }
+        else {
             $('#chat ul').css('background-color', 'inherit');
 
             clearInterval(spammer);
         }
     });
-    
+
 });
