09f2345d4ba0f2dc9101300c9f3b69cc64785667
HerrCraziDev <herrcrazi@gmail.com>
2018-03-21 17:52:59 +0100

    Serveur du chat

---
+++ kitten/server.js
@@ -0,0 +1,23 @@
+const WS = require('ws');
+
+//Create the server
+const websocket = new WS.Server( {
+    port: 12345
+});
+console.log('Server is listening on port ' + websocket.port);
+
+websocket.on('connection', (client) => {
+    //New client
+    client.send('Bienvenue !');
+    console.log('New client connected');
+
+    client.on('message', (data) => {
+        //Broadcast to all
+        for ( let client in websocket.clients )
+        {
+            client.send(data);
+        }
+
+        console.log(data);
+    });
+});
