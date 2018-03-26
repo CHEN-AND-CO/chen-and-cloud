5b84ff911991aae7630a3667390b9953dd1e8ebd
Gwenolé <gwenole.lrf.29@gmail.com>
2018-03-26 16:41:46 +0200

    truc

--- kitten/client.js
+++ kitten/client.js
@@ -1,5 +1,7 @@
 const WS = require('ws');
 
+parseArgs();
+
 const websocket = new WebSocket('ws://localhost:'+ settings.port);
 
 websocket.on('open', () => {
@@ -13,3 +15,28 @@ websocket.on('message', (data) => {
 websocket.on('error', () => {
     console.log('Failed to connect to ' + websocket.url);
 });
+
+
+function parseArgs()
+{
+    //Command line arguments parsing
+    for (var i = 2; i < process.argv.length; i++)
+    {
+        switch (process.argv[i])
+        {
+            case '-p':
+            case '--port':
+                settings.port = parseInt(process.argv[i + 1]);
+                i++;
+                break;
+
+            case '-h':
+            case '--help':
+                console.log("Small and ready-to-use websocket server. By G. Leroy-Ferrec (HerrCrazi)\nUsage : \n\t-h, --help : Display this help.\n\nServer settings :\n\t-p <port>, --port <port> : Listen on the given <port>.");
+                break;
+
+            default:
+                console.warn('Unknown argument \'' + process.argv[i] + '\'');
+        }
+    }
+}
