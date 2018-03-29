88eef2fbeeef8584bbf53e9b28b7a1ae118fbff4
BlivionIaG <kev29lt@gmail.com>
2018-03-29 15:41:25 +0200

    modified:   js/authentication.js 	modified:   php/request.php

--- php/request.php
+++ php/request.php
@@ -1,6 +1,8 @@
 <?php
+    /* Inclusion des constantes du projet */
     require_once('database.php');
 
+    /* Envoi des données sous format JSON avec un header personnalisé */
     function sendJsonData($message, $h){
         header($h);
         header('Content-Type: text/plain; charset=utf-8');
@@ -10,17 +12,23 @@
         echo json_encode($message);
     }
 
+    /* Authentification */
     function authenticate($db){
         $login = $_SERVER['PHP_AUTH_USER'];
         $pass = $_SERVER['PHP_AUTH_PW'];
 
+        /* Vérifie l'utilisateur */
         if(!dbCheckUserInjection($db, $login, $pass)){
             header('HTTP/1.1 401 Unauthorized');
             exit;
         }
 
+        /* Génération du token */
         $token = base64_encode(openssl_random_pseudo_bytes(12));
+
+        /* Ajout du token */
         dbAddToken($db, $login, $token);
+
         header('Content-Type: text/plain; charset=utf-8');
         header('Cache-control: no-store, no-cache, must-revalidate');
         header('Pragma: no-cache');
@@ -29,15 +37,22 @@
         exit;
     }
 
+    /* Vérification du token */
     function verifyToken($db){
+        /* Récupère les headers HTTP */
         $headers = getallheaders();
+
+        /* Récupère le token */
         $token = $headers['Authorization'];
+        
+        /* Vérifie le token et récupère le login */
         if (preg_match('/Bearer (.*)/', $token, $tab))$token = $tab[1];
         if(!($login = dbVerifyToken($db, $token))){
             header('HTTP/1.1 401 Unauthorized');
             exit;
         }
 
+        /* Renvoie le login */
         return $login;
     }
 
