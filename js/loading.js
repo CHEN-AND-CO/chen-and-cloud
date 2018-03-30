f93eb3f232a8a40bc19651289eb252a58af433d0
BlivionIaG <kev29lt@gmail.com>
2018-03-30 21:29:54 +0200

    modified:   js/loading.js

--- js/loading.js
+++ js/loading.js
@@ -5,12 +5,10 @@
 loadPhotos(ajaxResponse:String)
 Charge la liste des miniatures des photos, et ajoute les binds appropriés sur les miniatures
 */
-function loadPhotos(ajaxResponse)
-{
+function loadPhotos(ajaxResponse) {
     var photos = JSON.parse(ajaxResponse);
     //Ajout des miniatures à la page
-    for (const i in photos)
-    {
+    for (const i in photos) {
         var balise = document.createElement('div');
         balise.innerHTML = '<a href="#"><img class="thumbnail" src="' + photos[i].src + '" id="photo-' + photos[i].id + '"></a>';
         $('#thumbnails').append(balise);
@@ -35,8 +33,7 @@ function loadPhotos(ajaxResponse)
 loadPhoto(ajaxResponse:String)
 Charge une grande photo et l'insére dans la page
 */
-function loadPhoto(ajaxResponse)
-{
+function loadPhoto(ajaxResponse) {
     var photo = JSON.parse(ajaxResponse);
     var balise = '<h2>' + photo[0].title + '</h2>';
     balise += '<a href="#" class="thumbnail"><img src="' + photo[0].src + '"></a>';
@@ -50,16 +47,14 @@ function loadPhoto(ajaxResponse)
 loadComments(ajaxResponse:String)
 Affiche les commentaires associés à la photo sélectionnée sur la page
 */
-function loadComments(ajaxResponse)
-{
+function loadComments(ajaxResponse) {
     var comments = JSON.parse(ajaxResponse);
     console.log(comments);
 
     $('#comments .msg-list').html("");  //Suppression des commentaires précédents s'il y en a
 
     //Ajout de chaque commentaire
-    for (const i in comments)
-    {
+    for (const i in comments) {
         var balise = document.createElement('div');
         console.log(comments[i].userLogin);
 
@@ -68,7 +63,7 @@ function loadComments(ajaxResponse)
         comment += comments[i].userLogin;
         comment += '</span>';
         //Si l'utilisateur connecté est propriétaire du commentaire, y ajouter un bouton de suppression dudit commentaire
-        if ( comments[i].userLogin === Cookies.get('login') ) comment += '<button class="comment-delete" id="delete-' + comments[i].id + '">×</button>';
+        if (comments[i].userLogin === Cookies.get('login')) comment += '<button class="comment-delete" id="delete-' + comments[i].id + '">×</button>';
         comment += '</div>' + comments[i].comment + '</li>';
 
         balise.innerHTML = comment;
@@ -76,15 +71,13 @@ function loadComments(ajaxResponse)
 
         //Gestion du clic sur le bouton de suppression
         $('#delete-' + comments[i].id).unbind('click').click(
-            function (event)
-            {
+            function (event) {
                 var id = event.target.id.substr(7);
                 var photoId = $('#photo').attr('photoid');
                 event.preventDefault();
 
                 //Suppression du commentaire, et actualisation de la liste des commentaires
-                if (id != undefined && photoId != undefined)
-                {
+                if (id != undefined && photoId != undefined) {
                     ajaxRequest('DELETE', 'php/request.php/comments/', function () {
                         ajaxRequest('GET', 'php/request.php/comments/', loadComments, 'id=' + photoId);
                     }, 'id=' + id);
@@ -100,10 +93,9 @@ function loadComments(ajaxResponse)
         var comment = $('#comment-msg').val();
         var photoId = $('#photo').attr('photoid');
         event.preventDefault();
-        
+
         //Ajout du commentaire à la BDD et actualisation des commentaires
-        if (comment != '' && photoId != undefined)
-        {
+        if (comment != '' && photoId != undefined) {
             ajaxRequest('POST', 'php/request.php/comments/', () => {
                 ajaxRequest('GET', 'php/request.php/comments/', loadComments, 'id=' + photoId);
             }, 'id=' + photoId + '&comment=' + comment);
@@ -119,15 +111,13 @@ var nbItemsLoaded = 0;  //Nombre de photos à charger
 showPhotosAnim(nbItems:integer)
 Affiche les miniatures avec une animation de fade-in une fois toutes les miniatures chargées
 */
-function showPhotosAnim(nbItems)
-{
+function showPhotosAnim(nbItems) {
     nbItemsLoaded++;    //Une miniature supplémentaire a été chargée*
 
     console.log(nbItemsLoaded + ' loaded out of ' + nbItems);
 
     //Si toutes les photos ont été chargées, lancement de l'animation
-    if (nbItemsLoaded >= nbItems)
-    {
+    if (nbItemsLoaded >= nbItems) {
         $('.thumbnail').each((i) => {
             $('.thumbnail#photo-' + (i + 1)).css('transition-delay', (i / 40) + 's').css('opacity', '1');   //transition-delay est utilisé pour faire apparaitre de maniére progressive les photos
         });
